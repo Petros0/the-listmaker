d117a7d7a4a5489b1eff24d8110919bae819df16
Petros0 <petros.stergioulas94@gmail.com>
2019-05-05 20:37:49 +0200

    :sparkles: create the master view

--- app/src/main/java/com/stergioulas/thelistmaker/MainActivity.kt
+++ app/src/main/java/com/stergioulas/thelistmaker/MainActivity.kt
@@ -3,23 +3,25 @@ package com.stergioulas.thelistmaker
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
     private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+                textMessage.setText(R.string.title_agenda)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+                textMessage.setText(R.string.title_lists)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+                textMessage.setText(R.string.title_settings)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -29,6 +31,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         textMessage = findViewById(R.id.message)
