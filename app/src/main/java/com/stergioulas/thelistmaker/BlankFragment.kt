b6d560edea7632f50abdb08043d6401fa967aded
Petros0 <petros.stergioulas94@gmail.com>
2019-05-06 20:59:27 +0200

    :lipstick: create the List fragment, recycle view, still needs work.

--- app/src/main/java/com/stergioulas/thelistmaker/BlankFragment.kt
+++ app/src/main/java/com/stergioulas/thelistmaker/BlankFragment.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,25 +28,32 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class BlankFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
     private var listener: OnFragmentInteractionListener? = null
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        val view = inflater.inflate(R.layout.fragment_blank_recycler, container, false)
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        val listOf = listOf("Bla", "bla", "bla", "bla")
+
+        val adapter = ListAdapter(listOf)
+
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, layoutManager.orientation))
+
+        return view
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -77,22 +88,32 @@ class BlankFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = BlankFragment()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var textViewDayOfWeek: TextView = itemView.findViewById<View>(R.id.day_of_week_textview) as TextView
+        internal var textviewDateNumber: TextView = itemView.findViewById<View>(R.id.date_number_textview) as TextView
+    }
+
+    class ListAdapter(private val list: List<String>) : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+
+            val view = LayoutInflater.from(p0.context).inflate(R.layout.fragment_blank_recycler_item, p0, false)
+
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return this.list.size
+        }
+
+        override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+
+            p0.textViewDayOfWeek.text = "Bla"
+            p0.textviewDateNumber.text = "Blo"
+        }
     }
 }
